1a19e9b46181ecf063a036ecec2bbf659ab8e647
Harry Lin <harrylin@thinktronltd.com>
2024-03-15 18:36:48 +0800

    improve accessibility, add authentication and metadata

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,8 +1,13 @@
+import { Metadata } from 'next';
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
- 
+
+export const metadata: Metadata = {
+  title: 'Edit Invoice',
+};
+
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
   const [invoice, customers] = await Promise.all([
